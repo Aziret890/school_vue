ca642230674df1902172cf1cffac8cff6f9757d0
aziret <kubaaziret68@gmail.com>
2024-04-30 18:45:37 +0600

    aziret

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,11 @@
 </template>
 <script setup>
 import Header from "./components/Header.vue";
+import { onMounted } from "vue";
+import AOS from "aos";
+
+onMounted(() => {
+  AOS.init();
+});
 </script>
 <style scoped></style>
